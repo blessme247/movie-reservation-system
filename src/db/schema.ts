531795a160071327ad6a1d941bbee681addf9527
blessme247 <solabiabraham@gmail.com>
2026-05-10 00:51:12 +0100

    feat: implement complete schema setup with db migrations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, varchar, uniqueIndex, index, timestamp } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, uniqueIndex, index, date, time, timestamp, AnyPgColumn, } from "drizzle-orm/pg-core";
 import { timestamps } from "./columns.helpers";
 
 export const usersTable = pgTable("users", {
@@ -17,22 +17,128 @@ export const usersTable = pgTable("users", {
 export const moviesTable = pgTable("movies", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   title: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 500 }).notNull(),
+  runTime: integer().notNull(),
+  releaseDate: date().notNull(),
+  posterImageId: integer().references((): AnyPgColumn => posterImagesTable.id),
+  statusId: integer().references((): AnyPgColumn => movieStatusTable.id),
   ...timestamps
 },
 (table) => [
-  index("title_idx").on(table.title)
+  index("title_idx").on(table.title),
+  index("status_idx").on(table.statusId),
 ]
 );
 
 export const showTimesTable = pgTable("showtimes", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   movieId: integer().references(() => moviesTable.id),
-  startTime: timestamp(),
-  endTime: timestamp(),
+  cinemaId: integer().references(() => cinemaTable.id),
+  scheduleId: integer().references(() => scheduleTable.id),
+  totalReservations: integer().notNull(),
+  datetime: timestamp().notNull(),
   ...timestamps
 });
+
+
+export const scheduleTable = pgTable("schedules", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+});
+
+export const cinemaTable = pgTable("cinemas", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  capacity: integer().notNull()
+},
+(table)=> [
+  (index("name_idx").on(table.name))
+]);
 
 export const rolesTable = pgTable("roles", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
 });
+
+export const posterImagesTable = pgTable("poster_images", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  movieId: integer().references(() => moviesTable.id),
+  publicId: varchar({length: 255}).notNull(),
+  assetId: varchar({length: 255}).notNull(),
+  secureUrl: varchar({length: 1000}).notNull(),
+  width: integer().notNull(),
+  height: integer().notNull(),
+  format: varchar({length: 100}).notNull(),
+  resourceType: varchar({length: 100}).notNull(),
+  folder: varchar({length: 100}).notNull(),
+  bytes: integer().notNull(),
+  ...timestamps
+},
+(table) => [
+  index("movie_id_idx").on(table.movieId)
+]
+);
+
+export const movieGenresTable = pgTable("genres", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  movieId: integer().references(() => moviesTable.id),
+  name: varchar({ length: 255 }).notNull(),
+})
+
+export const seatReservationsTable = pgTable("seat_reservations", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  showTimeId: integer().references(() => showTimesTable.id),
+  userId: integer().references(() => usersTable.id),
+  ticketTypeId: integer().references(() => ticketTypesTable.id),
+  ...timestamps
+}, (table) => [
+  index('showtime_id_idx').on(table.showTimeId),
+  index('user_id_idx').on(table.userId),
+])
+
+export const ticketTypesTable = pgTable("ticket_types", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+});
+
+export const movieStatusTable = pgTable("status", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+});
+
+export const seatsTable = pgTable("seats", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  cinemaId: integer().references(() => cinemaTable.id),
+  seatNumber: varchar({ length: 10}).notNull()
+})
+
+export const reservationTicketsTable = pgTable("reservation_tickets", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  reservationId: integer().references(() => seatReservationsTable.id),
+  ticketTypeId: integer().references(() => ticketTypesTable.id),
+  ...timestamps
+},
+(table) => [
+  index('reservation_id_idx').on(table.reservationId),
+])
+
+export const reservationSeatsTable = pgTable("reservation_seats", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  reservationId: integer().references(() => seatReservationsTable.id),
+  seatId: integer().references(() => seatsTable.id),
+  ...timestamps
+},
+(table) => [
+  index('reservation_id_idx_seats').on(table.reservationId),
+])
+
+
+export const showtimeReservedSeatsTable = pgTable("showtime_reserved_seats", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  showTimeId: integer().references(() => showTimesTable.id),
+  seatId: integer().references(() => seatsTable.id),
+  ...timestamps
+}, (table) => [
+  index('showtime_id_idx_reserved_seats').on(table.showTimeId),
+  index('seat_id_idx').on(table.seatId),
+])
